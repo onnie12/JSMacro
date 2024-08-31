658888493b36122050861134e030df20d126f778
Onni <152686116+onnie12@users.noreply.github.com>
2024-08-31 19:36:30 +0200

    minor changes

--- src/main.js
+++ src/main.js
@@ -2,20 +2,22 @@
 let chars = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 let inputNum = document.getElementById("inputNumHere");
 let inputMouse = document.getElementById("inputMouseHere");
+const errorText = document.getElementById("errorText");
 
 function startMacro(){
+    let holdButton = document.getElementById("holdBtn");
     let inputNum = document.getElementById("inputNumHere");
-    if(inputNum.value == 1){
-        console.log("works");
+    if(inputNum.value == "1"){
+        startMacro();
     }
     else{
-        console.log("Sorry, Your Input is not a valid number.");
+        errorText.textContent = "Sorry, Your Input is not a valid number.";
     }
     let inputMouse = document.getElementById("inputMouseHere");
-    if(inputMouse.value == "M1"){
-        console.log("works");
+    if(inputMouse.value == "M1", "M2"){
+        startMacro();
     }
     else{
-        console.log("Sorry, That Mouse Button does not seem to exist.");
+        errorText.textContent = "Sorry, That Mouse Button does not seem to exist.";
     }
 }
